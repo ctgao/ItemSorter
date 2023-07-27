007ce8e7dc3ddde69041cbb07834926d29a8e116
Christine <christine@zipcodersMBP13.localdomain>
2023-07-27 09:14:46 -0400

    finished the lab - also modified the pom file

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,41 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+    private Long idNumber;
+    private String name;
+    private Double price;
+
     public Item() {
+        this(0L, "", 0.01);
     }
 
     public Item(Long id, String name, Double price) {
+        idNumber = id;
+        this.name = name;
+        this.price = price;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Long getId() {
-        return null;
+        return idNumber;
     }
 
     public void setId(Long id) {
+        idNumber = id;
     }
 
     public Double getPrice() {
-        return null;
+        return price;
     }
 
     public void setPrice(Double price) {
+        this.price = price;
     }
 }
--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,22 @@
 package rocks.zipcode.io;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    private Item[] items;
+
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        ArrayList<Item> unsorted = new ArrayList<>(Arrays.asList(items));
+        unsorted.sort(comparator);
+        return unsorted.toArray(new Item[items.length]);
     }
 }
